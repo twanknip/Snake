aa23f391849176ec7780e6b1d328239d13f4db53
twanknip <twanknip@gmail.com>
2024-01-16 17:24:10 +0100

    Add project files.

---
+++ Snake/GameState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class GameState
+    {
+        public int Rows { get; }
+        public int Cols { get; }
+        public GridValue[,] Grid {  get; }
+        public Direction Dir { get; private set; }
+        public int Score { get; private set; }
+        public bool GameOver { get; private set; }
+
+        private readonly LinkedList<Position> snakePositions = new LinkedList<Position>();
+        private readonly Random random = new Random();
+
+        public GameState(int rows, int cols)
+        {
+
+            Rows = rows;
+            Cols = cols;
+            Grid = new GridValue[rows, cols];
+        }
+
+
+    }
+}
