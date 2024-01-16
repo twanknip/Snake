d3dfc448095ee4375e479fa792104fd4dc9760e3
twanknip <twanknip@gmail.com>
2024-01-16 17:48:11 +0100

    Begin GameState 1.0

--- Snake/GameState.cs
+++ Snake/GameState.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Printing;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,7 @@ namespace Snake
     {
         public int Rows { get; }
         public int Cols { get; }
-        public GridValue[,] Grid {  get; }
+        public GridValue[,] Grid { get; }
         public Direction Dir { get; private set; }
         public int Score { get; private set; }
         public bool GameOver { get; private set; }
@@ -20,12 +21,50 @@ namespace Snake
 
         public GameState(int rows, int cols)
         {
-
             Rows = rows;
             Cols = cols;
             Grid = new GridValue[rows, cols];
+            Dir = Direction.Right;
+            AddSnake();
         }
 
+        private void AddSnake()
+        {
+            int r = Rows / 2;
 
+            for (int c = 1; c <= 3; c++)
+            {
+                Grid[r, c] = GridValue.Snake;
+                snakePositions.AddFirst(new Position(r, c));
+            }
+        }
+
+        private IEnumerable<Position> EmptyPositions()
+        {
+            for (int r = 0; r < Rows; r++)
+            {
+                for (int c = 0; c < Cols; c++)
+                {
+                    if (Grid[r, c] == GridValue.Empty)
+                    {
+                        yield return new Position(r, c);
+                    }
+                }
+            }
+        }
+
+        private void AddFood()
+        {
+            List<Position> empty = new List<Position>(EmptyPositions());
+
+
+            if (empty.Count == 0)
+            {
+                return;
+            }
+
+            Position pos = empty[random.Next(empty.Count)] ;
+            Grid[pos.Row, pos.Col] = GridValue.Food;
+        }
     }
 }
